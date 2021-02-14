15b86bca4eabb34f1bad6c715850be9b8412f04e
Joshua Ong <joshua.c.ong@hotmail.com>
2021-02-14 09:46:55 +0000

    Fixed animations on home page

--- script.js
+++ script.js
@@ -23,6 +23,37 @@ This is the main JavaScript file for Code-Ed. This file handles operations like
 //API_KEY from Google Credentials
 //let API_KEY = "AIzaSyBTrwHONkXik6Dfm6XnMC3rda3IBsl0Ys4"
 
+function authenticate() {
+    return gapi.auth2.getAuthInstance()
+        .signIn({scope: "https://www.googleapis.com/auth/youtube.force-ssl"})
+        .then(function() { console.log("Sign-in successful"); },
+              function(err) { console.error("Error signing in", err); });
+  }
+  function loadClient() {
+    gapi.client.setApiKey("AIzaSyBTrwHONkXik6Dfm6XnMC3rda3IBsl0Ys4");
+    return gapi.client.load("https://www.googleapis.com/discovery/v1/apis/youtube/v3/rest")
+        .then(function() { console.log("GAPI client loaded for API"); },
+              function(err) { console.error("Error loading GAPI client for API", err); });
+  }
+  // Make sure the client is loaded and sign-in is complete before calling this method.
+  function execute() {
+    return gapi.client.youtube.search.list({
+      "part": [
+        "snippet"
+      ],
+      "maxResults": 25,
+      "q": "surfing"
+    })
+        .then(function(response) {
+                // Handle the results here (response.result has the parsed body).
+                console.log("Response", response);
+              },
+              function(err) { console.error("Execute error", err); });
+  }
+  gapi.load("client:auth2", function() {
+    gapi.auth2.init({client_id: "989850531815-3dr7nh4nq6ggtna0l7kmkcqs3pposiji.apps.googleusercontent.com"});
+  });
+
 //Function to go to the home page when the Home button is selected
 function goHome(){
   window.location="/index.html";
@@ -41,7 +72,6 @@ function goChallenges(){
 //Function to go to the Python page when the Python button is selected
 function goPython(){
   window.location="/python.html";
-  execute();
   //videoSearch(API_KEY, "Python", 9)
 }
 
@@ -123,33 +153,4 @@ function goC(){
    * https://developers.google.com/explorer-help/guides/code_samples#javascript
    */
 
-  function authenticate() {
-    return gapi.auth2.getAuthInstance()
-        .signIn({scope: "https://www.googleapis.com/auth/youtube.force-ssl"})
-        .then(function() { console.log("Sign-in successful"); },
-              function(err) { console.error("Error signing in", err); });
-  }
-  function loadClient() {
-    gapi.client.setApiKey("AIzaSyBTrwHONkXik6Dfm6XnMC3rda3IBsl0Ys4");
-    return gapi.client.load("https://www.googleapis.com/discovery/v1/apis/youtube/v3/rest")
-        .then(function() { console.log("GAPI client loaded for API"); },
-              function(err) { console.error("Error loading GAPI client for API", err); });
-  }
-  // Make sure the client is loaded and sign-in is complete before calling this method.
-  function execute() {
-    return gapi.client.youtube.search.list({
-      "part": [
-        "snippet"
-      ],
-      "maxResults": 25,
-      "q": "surfing"
-    })
-        .then(function(response) {
-                // Handle the results here (response.result has the parsed body).
-                console.log("Response", response);
-              },
-              function(err) { console.error("Execute error", err); });
-  }
-  gapi.load("client:auth2", function() {
-    gapi.auth2.init({client_id: "YOUR_CLIENT_ID"});
-  });
+  
