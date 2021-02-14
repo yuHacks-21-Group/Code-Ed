79f0cfaecd21d03177f43651776d7d3232b231fa
Nathan DeGoey <n.degoey@mail.utoronto.ca>
2021-02-14 05:34:46 +0000

    Adding youtube videos and more styling

--- script.js
+++ script.js
@@ -4,30 +4,30 @@ This is the main JavaScript file for Code-Ed. This file handles operations like
 
 //Function to go to the home page when the Home button is selected
 function goHome(){
-  window.location="index.html";
+  window.location="/index.html";
 }
 
 //Function to go to the About Us page when the About Us button is selected
 function goAbout(){
-  window.location="about.html";
+  window.location="/about.html";
 }
 
 //Function to go to the More Challenges page when the More Challenges button is selected
 function goChallenges(){
-  window.location="challenges.html";
+  window.location="/challenges.html";
 }
 
 //Function to go to the Python page when the Python button is selected
 function goPython(){
-  window.location="python.html";
+  window.location="/python.html";
 }
 
 //Function to go to the Java page when the Java button is selected
 function goJava(){
-  window.location="java.html";
+  window.location="/java.html";
 }
 
 //Function to go to the C page when the C button is selected
 function goC(){
-  window.location="c.html";
+  window.location="/c.html";
 }
